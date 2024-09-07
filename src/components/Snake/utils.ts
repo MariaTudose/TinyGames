352ab3bdc1cb2a363bfedf261b3566a33c1dfc14
Maria Tudose <dev.tudose@gmail.com>
2024-09-07 21:49:38 +0300

    Don't spawn apples too close to the head

--- src/components/Snake/utils.ts
+++ src/components/Snake/utils.ts
@@ -3,10 +3,15 @@ export const getRandomPos = () => Math.ceil(Math.random() * (n - 1));
 export const getRandomCoords = () => [getRandomPos(), getRandomPos()];
 export const modN = (newPos: number) => ((((newPos - 1) % n) + n) % n) + 1;
 
+const overlapsSnake = (newCoords: number[], coords: number[][]) =>
+	coords.find(([y, x]) => y === newCoords[0] && x === newCoords[1]);
+const tooClose = (newCoords: number[], coords: number[][]) =>
+	Math.abs(coords[0][0] - newCoords[0]) <= 3 && Math.abs(coords[0][1] - newCoords[1]) <= 3;
+
 export const getFoodCoords = (coords: number[][]) => {
 	let newCoords: number[];
 	do {
 		newCoords = getRandomCoords();
-	} while (coords.find(([y, x]) => y === newCoords[0] && x === newCoords[1]));
+	} while (overlapsSnake(newCoords, coords) || tooClose(newCoords, coords));
 	return newCoords;
 };
